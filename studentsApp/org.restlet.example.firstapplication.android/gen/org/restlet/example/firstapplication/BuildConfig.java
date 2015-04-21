9017763614d596b72a4300683e2344234f2e97fb
hungcute1688 <hungcv1688@gmail.com>
2015-04-21 10:44:58 +0100

    Draft version
    We will finish the residual stuffs later

---
+++ studentsApp/org.restlet.example.firstapplication.android/gen/org/restlet/example/firstapplication/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.restlet.example.firstapplication;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
